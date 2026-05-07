6fda28de922c99528b4bb08b19df71ea4fa2336b
odaniel <danielmenezes0822@gmail.com>
2026-05-07 19:54:07 -0400

    feat: add SIDE and FACE enums for directional representation

---
+++ src/core/engine/enum.ts
@@ -0,0 +1,15 @@
+export enum SIDE {
+    TOP = "top",
+    BOTTOM = "bottom",
+    LEFT = "left",
+    RIGHT = "right",
+}
+
+export enum FACE {
+    FRONT = "front",
+    BACK = "back",
+    LEFT = "left",
+    RIGHT = "right",
+    TOP = "top",
+    BOTTOM = "bottom",
+}
