1388d8ce8cab00bd7c91bf0d3ce807c96ed8cfe3
odaniel <danielmenezes0822@gmail.com>
2026-05-07 19:54:11 -0400

    feat: add types for Degree, Pixel, and Color representations

---
+++ src/core/engine/types.ts
@@ -0,0 +1,4 @@
+
+export type Degree = `${number}deg`;
+export type Pixel = `${number}px`;
+export type Color = `#${string}` | `rgb(${number}, ${number}, ${number})` | `rgba(${number}, ${number}, ${number}, ${number})` | `hsl(${number}, ${number}%, ${number}%)` | `hsla(${number}, ${number}%, ${number}%, ${number})`;
